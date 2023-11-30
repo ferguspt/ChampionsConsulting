e53617ba4ef6c3aafb284e1b68d775288a73f443
brow25mc <mcbrown0506@gmail.com>
2023-11-30 16:19:57 -0500

    More progress on User selection information

--- ChampionsConsulting/Pages/AdminPages/UserInformation.cshtml.cs
+++ ChampionsConsulting/Pages/AdminPages/UserInformation.cshtml.cs
@@ -1,3 +1,4 @@
+using ChampionsConsulting.Pages.DataClasses;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,13 @@ namespace ChampionsConsulting.Pages.AdminPages
 {
     public class UserInformationModel : PageModel
     {
+        [BindProperty]
+        public List<UserDataAll> Data { get; set; }
+
+        public UserInformationModel()
+        {
+            Data = new List<UserDataAll>();
+        }
         public void OnGet()
         {
         }
